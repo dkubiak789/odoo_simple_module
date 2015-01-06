e606058c9a16442682a7a780e4baddaa49c4c9b1
vagrant <vagrant@precise32.(none)>
2015-01-06 12:15:21 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+odoo_simple_module
+==================
+
+Simple Odoo module with simple list and form view.
+Use it to create new modules easier.
---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Dariusz Kubiak, dkubiak789@wp.pl
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Odoo Simple Module',
+ 'description': """
+Just a simple Odoo Module
+=========================
+
+Key features:
+-------------
+    * Works great
+    * Looks good
+    * No bugs
+    * Great support
+""",
+ 'version': '1.1',
+ 'author': 'Dariusz Kubiak, dkubiak789@wp.pl',
+ 'category': 'Simple example',
+ 'data': ['simple_model_view.xml'],
+ 'auto_install': False,
+ 'installable': True,
+ }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
